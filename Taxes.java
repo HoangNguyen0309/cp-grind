91858443dd4d9fa317a580abe07cefd2dca90fd6
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-10-20 10:52:39 -0400

    Add files via upload

---
+++ Taxes.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Taxes {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        long input = myObj.nextInt();
+}
